62b4e23f3b7e168ddce1fe323279640b1fd8d509
Marcelo Zepeda <marcelo@fabricadecodigo.cl>
2023-06-23 23:14:49 -0400

    modelos en admin

--- blog/principal/admin.py
+++ blog/principal/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from principal.models import Publicacion
 
+class PublicacionAdmin(admin.ModelAdmin):
+  list_display = ['titulo', 'fecha_creacion', 'fecha_publicacion', 'autor']
+  list_filter = ['fecha_creacion', 'fecha_publicacion', 'autor']
+  search_fields = ['titulo', 'contenido']
+  ordering = ['fecha_creacion', 'fecha_publicacion']
+  fields = ['titulo', 'resumen', 'slug', 'contenido', 'fecha_creacion', 'fecha_publicacion', 'autor']
+  readonly_fields = ['fecha_creacion', 'fecha_publicacion']
+
+
+
+admin.site.register(Publicacion, PublicacionAdmin)
 # Register your models here.
