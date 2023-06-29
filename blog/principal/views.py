bc7a10f8f735fb040b1a1aedc05ab158344e59e4
Marcelo Zepeda <marcelo@fabricadecodigo.cl>
2023-06-29 10:04:26 -0400

    envío de correo

--- blog/principal/views.py
+++ blog/principal/views.py
@@ -1,8 +1,9 @@
+import os
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from principal.models import Publicacion, Comentario, Visita
 from django.contrib.auth.models import Group, User
-
+from django.core.mail import send_mail
 # Create your views here.
 
 class Muestra(TemplateView):
@@ -19,4 +20,19 @@ class Muestra(TemplateView):
     visita = Visita(publicacion=publicacion)
     visita.save()
     # context["post"] = { "titulo": publicacion.titulo, "contenido": publicacion.contenido}
+
+    mensaje = """
+Este mensaje es para probar el envío de correos.
+Debiera haber un salto de línea aquí.
+Esta es otra línea.
+lorem ipsum dolor sit amet
+    """
+
+    send_mail(
+      '[TE LO VENDO] - Prueba',
+      mensaje,
+      os.environ.get('EMAIL_HOST_USER'),
+      ['profezepeda@outlook.com'], fail_silently=False
+    )
+
     return render(request, self.template_name, context)
